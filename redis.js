3f1da6729b32cd0e4625efc2d60305265e87732a
Krishnan Family <pavik546@gmail.com>
2025-01-17 19:50:19 +0530

    commit

--- redis.js
+++ redis.js
@@ -2,7 +2,7 @@ const redis = require('redis');
 
 // Create Redis client
 const client = redis.createClient({socket: {
-    host: process.env.REDIS_HOST || 'localhost',
+    host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || 6379
   }});
 
